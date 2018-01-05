1f1a45243e0b9cec131c2d69e4945831290d29c0
Paraxic <paraxic@outlook.com>
2018-01-04 21:41:10 -0500

    chop now chops line # (lines start at 0), update yt_link to fix url replication

--- chop.c
+++ chop.c
@@ -12,27 +12,19 @@ int main(int argc, char * argv[])
 		fprintf(stderr,"File Name and Count required\n");
 		exit(0);
 	}
-	char * prefix = NULL;
-	if(argc > 3)
-	{ 
-	  prefix = malloc(sizeof(char) * 100);
-	  strncpy(prefix,argv[3],strlen(argv[3])+1); 
-	}
 	FILE * fp = fopen(argv[1],"r");
-	size_t print = atoi(argv[2]);
+	size_t print = (size_t)atoi(argv[2]);
 	size_t printed = 0;
 	size_t sz;
 	char * line= malloc(sizeof(char) * 100);
 	while((getline(&line,&sz,fp) != -1)){
-		if(printed == print){
-			break;
-		}
-		if(prefix != NULL){
-		  printf("%s%s",prefix,line);
-		}else if(prefix == 0){
-			printf("%s",line);
-		}
-		printed++;
+	  if(printed != print){
+		  printed++;
+		  continue;
+	  } else if(printed == print){
+		  printf("https://www.youtube.com%s\n",line);
+		  break;
+	  }
 	}
 	return 0;
 }
